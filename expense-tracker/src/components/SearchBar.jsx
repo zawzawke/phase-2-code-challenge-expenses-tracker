5fd396c210afdf7f26b7d68bbd5287761b7d35e2
Paul <paulallanmain@gmail.com>
2025-04-14 15:05:53 +0300

    add searchbar

--- expense-tracker/src/components/SearchBar.jsx
+++ expense-tracker/src/components/SearchBar.jsx
@@ -1,3 +1,20 @@
+import React from 'react';
+import './SearchBar.css';
+
+function SearchBar({ searchTerm, onSearchChange }) {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search by name or description..."
+        value={searchTerm}
+        onChange={(e) => onSearchChange(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
 
 
 
