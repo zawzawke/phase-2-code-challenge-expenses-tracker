1297f313b20ba93667f7f21fc4b42496ced72be2
Paul <paulallanmain@gmail.com>
2025-04-15 00:33:34 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     <>
     <header>
       <h1>Expense Tracker</h1>
-      <p>Start taking control of your finanaces and life. Record, categorize and analyze your spending</p>
+      <p>Start taking control of your finances and life. Record, categorize and analyze your spending</p>
     </header>
       <div className="app-container">
           <ExpenseForm onAddExpense={handleAddExpense} />
